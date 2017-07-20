00057098165694d7b422ebf4338e92aca4d60906
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-07-20 12:25:31 +0545

    Before new

--- app/src/main/java/np/com/manandharbibesh/finalapp/MainActivity.java
+++ app/src/main/java/np/com/manandharbibesh/finalapp/MainActivity.java
@@ -3,17 +3,18 @@ package np.com.manandharbibesh.finalapp;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import np.com.manandharbibesh.finalapp.databinding.AddBinding;
 import np.com.manandharbibesh.finalapp.model.AddModel;
 import np.com.manandharbibesh.finalapp.viewModel.AddPresenter;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AddBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.hello.setText("SDf");
         AddPresenter presenter = new AddPresenter();
         AddModel model = new AddModel();
         binding.setModel(model);
